9c7b21c59aca0f52f46990079f915c2ce7b06c0e
Ganesh Gunasegaran <me@itsgg.com>
2021-01-14 13:46:14 +0530

    Dont precompile graphiql assets in non dev

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module PostmanTestApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.precompile += %w( graphiql/rails/application.js graphiql/rails/application.css ) if Rails.env.development?
   end
 end
